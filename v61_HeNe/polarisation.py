4334e986f468139e920cf4b0aac4d0478ae71dee
Lukas Fiene <lukas.fiene@tu-dortmund.de>
2025-10-23 15:29:56 +0200

    Polarisation Fit

---
+++ v61_HeNe/polarisation.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+
+deg, P, dP = np.genfromtxt("data/Polarisation.txt",skip_header=2, unpack=True)
+deg=deg*2*np.pi/360
+
+x=np.linspace(0,2*np.pi,100)
+
+def f(x,I,x_0):
+   return I*np.cos(x-x_0)**2
+
+params, covariance_matrix = curve_fit(f, deg, P, sigma=dP)
+
+uncertainties = np.sqrt(np.diag(covariance_matrix))
+
+print("Polarization fit params: ")
+for name, value, uncertainty in zip("Ix", params, uncertainties):
+    print(f"{name} = {value} ± {uncertainty}")
+
+
+fig, ax = plt.subplots(1, 1, layout="constrained")
+ax.errorbar(deg, P, yerr=dP,fmt="k." ,label="Measurement")
+ax.plot(x,f(x,*params),label="Fit")
+ax.legend(loc="best")
+
+fig.savefig("build/Polarisation.pdf")
