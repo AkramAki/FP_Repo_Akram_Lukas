80fd52a8b4c2fa271590f8fd67d7d0fc7675cbc0
Lukas Fiene <lukas.fiene@tu-dortmund.de>
2025-10-25 09:55:45 +0200

    Polarization labels and ticks

--- v61_HeNe/polarisation.py
+++ v61_HeNe/polarisation.py
@@ -22,6 +22,15 @@ for name, value, uncertainty in zip("Ix", params, uncertainties):
 fig, ax = plt.subplots(1, 1, layout="constrained")
 ax.errorbar(deg, P, yerr=dP,fmt="k." ,label="Measurement")
 ax.plot(x,f(x,*params),label="Fit")
+ax.set_xlabel(r"$\theta$")
+ax.set_ylabel(r"$P\,[\unit{\milli\W}]")
+
+# Radian ticks
+custom_ticks = [0, np.pi/2, np.pi, (3*np.pi)/2, 2*np.pi]
+custom_tick_labels = ['$0$', '$\pi/2$', '$\pi$', '$3\pi/2$', '$2\pi$']
+ax.set_xticks(custom_ticks)
+ax.set_xticklabels(custom_tick_labels)
+
 ax.legend(loc="best")
 
 fig.savefig("build/Polarisation.pdf")
