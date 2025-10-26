f16ae45282ea98da32206265340888d0d806a0d9
Lukas Fiene <lukas.fiene@tu-dortmund.de>
2025-10-26 20:20:42 +0100

    fixed error in plot labels

--- v61_HeNe/polarisation.py
+++ v61_HeNe/polarisation.py
@@ -23,11 +23,11 @@ fig, ax = plt.subplots(1, 1, layout="constrained")
 ax.errorbar(deg, P, yerr=dP,fmt="k." ,label="Measurement")
 ax.plot(x,f(x,*params),label="Fit")
 ax.set_xlabel(r"$\theta$")
-ax.set_ylabel(r"$P\,[\unit{\milli\W}]")
+ax.set_ylabel(r"$P\,[\unit{\milli\watt}]$")
 
 # Radian ticks
 custom_ticks = [0, np.pi/2, np.pi, (3*np.pi)/2, 2*np.pi]
-custom_tick_labels = ['$0$', '$\pi/2$', '$\pi$', '$3\pi/2$', '$2\pi$']
+custom_tick_labels = [r'$0$', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$']
 ax.set_xticks(custom_ticks)
 ax.set_xticklabels(custom_tick_labels)
 
