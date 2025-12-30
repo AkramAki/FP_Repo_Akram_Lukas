158be4dad62052f6cb698d95fe1cab2e138fde06
Lukas Fiene <lukas.fiene@tu-dortmund.de>
2025-12-30 02:35:10 +0100

    finished gold analysis and discussion

--- v42_stm/plot.py
+++ v42_stm/plot.py
@@ -1,2 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+# gold avg and std
+gold_heights = np.array([4.14, 4.86, 8.25, 5.21, 6.75, 11.55])
+gold_avg = np.mean(gold_heights)
+gold_std = np.std(gold_heights)
+
+# print results
+print(f"Gold Average Height: {gold_avg:.2f} nm")
+print(f"Gold Standard Deviation: {gold_std:.2f} nm")
